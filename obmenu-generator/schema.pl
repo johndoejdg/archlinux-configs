bef65291649f7a4005da7042f9c11f185cbe39c8
korvinko <john.doe.jtg@gmail.com>
2016-04-18 08:36:38 +0000

    menu updated

--- obmenu-generator/schema.pl
+++ obmenu-generator/schema.pl
@@ -30,9 +30,9 @@ our $SCHEMA = [
 
     #          COMMAND                 LABEL                ICON
     {item => ['xdg-open .',        'File Manager',      'file-manager']},
-    {item => ['xfce4-terminal',             'Terminal',          'terminal']},
-    {item => ['xdg-open http://',  'Web Browser',       'web-browser']},
-    {item => ['gmrun',             'Run command',       'system-run']},
+    {item => ['xfce4-terminal',    'Terminal',          'terminal']},
+    #{item => ['xdg-open http://',  'Web Browser',       'web-browser']},
+    #{item => ['gmrun',             'Run command',       'system-run']},
 
     {sep => 'Categories'},
 
@@ -76,40 +76,39 @@ our $SCHEMA = [
     #{sep       => undef},
 
     ## Custom advanced settings
-    {sep => undef},
-    {begin_cat => ['Advanced Settings', 'gnome-settings']},
+    #{sep => undef},
+    #{begin_cat => ['Advanced Settings', 'gnome-settings']},
 
         # Configuration files
-        {item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-source']},
-        {item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-source']},
+        #{item => ["$editor ~/.conkyrc",              'Conky RC',    'text-x-source']},
+        #{item => ["$editor ~/.config/tint2/tint2rc", 'Tint2 Panel', 'text-x-source']},
 
         # obmenu-generator category
-        {begin_cat => ['Obmenu-Generator', 'menu-editor']},
-            {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
-            {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
+        #{begin_cat => ['Obmenu-Generator', 'menu-editor']},
+           # {item => ["$editor ~/.config/obmenu-generator/schema.pl", 'Menu Schema', 'text-x-source']},
+           # {item => ["$editor ~/.config/obmenu-generator/config.pl", 'Menu Config', 'text-x-source']},
 
-            {sep  => undef},
-            {item => ['obmenu-generator -p',       'Generate a pipe menu',              'menu-editor']},
-            {item => ['obmenu-generator -s -c',    'Generate a static menu',            'menu-editor']},
-            {item => ['obmenu-generator -p -i',    'Generate a pipe menu with icons',   'menu-editor']},
-            {item => ['obmenu-generator -s -i -c', 'Generate a static menu with icons', 'menu-editor']},
-            {sep  => undef},
+        #    {sep  => undef},
+          #  {item => ['obmenu-generator -p',       'Generate a pipe menu',              'menu-editor']},
+          #  {item => ['obmenu-generator -s -c',    'Generate a static menu',            'menu-editor']},
+          #  {item => ['obmenu-generator -p -i',    'Generate a pipe menu with icons',   'menu-editor']},
+          #  {item => ['obmenu-generator -s -i -c', 'Generate a static menu with icons', 'menu-editor']},
+        #    {sep  => undef},
 
-            {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
-        {end_cat => undef},
+          #  {item => ['obmenu-generator -d', 'Refresh Icon Set', 'gtk-refresh']},
+      #  {end_cat => undef},
 
         # Openbox category
-        {begin_cat => ['Openbox', 'openbox']},
-            {item => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
-            {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
-            {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
-            {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
-        {end_cat => undef},
+        #{begin_cat => ['Openbox', 'openbox']},
+          #  {item => ['openbox --reconfigure',               'Reconfigure Openbox', 'openbox']},
+          #  {item => ["$editor ~/.config/openbox/autostart", 'Openbox Autostart',   'shellscript']},
+          #  {item => ["$editor ~/.config/openbox/rc.xml",    'Openbox RC',          'text-xml']},
+          #  {item => ["$editor ~/.config/openbox/menu.xml",  'Openbox Menu',        'text-xml']},
+       #{end_cat => undef},
 
-    {end_cat => undef},
-    {sep => undef},
+    #{end_cat => undef},
+    #{sep => undef},
 
-    # Openbox category
 #    {begin_cat => ['Power options', 'power']},
 #	{item => ['openbox --exit', 'Logout', 'system-log-out']},
 #    	{item => ['systemctl reboot', 'Reboot', 'start-here']},
